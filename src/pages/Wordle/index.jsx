46dc7740cd9b1d8326f72fab2af499d2474a5165
sharath <sharathdupati@gmail.com>
2023-08-19 21:24:01 +0530

    refactored

--- src/pages/Wordle/index.jsx
+++ src/pages/Wordle/index.jsx
@@ -1,93 +1,100 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+
+import { getRandomWord, wordToCharIdxMap } from "./words";
+
 import styles from "./index.module.scss";
-import { getRandomWord } from "./words";
+import "react-toastify/dist/ReactToastify.css";
 
 const WORD_LENGTH = 5;
 const NUMBER_OF_TRYS = 6;
-const DELAY_TO_RESET_GAME = 1000;
 
 const Wordle = () => {
   const [wordToGuess, setWordToGuess] = useState(getRandomWord());
-  const [guesses, setGuesses] = useState([]);
   const [currentGuess, setCurrentGuess] = useState("");
-  const wordToGuessSet = new Set([...wordToGuess]);
-  const wordToGuessSetWithIndex = new Set(
-    [...wordToGuess].map((char, idx) => `${char}-${idx}`)
+  const [guesses, setGuesses] = useState([]);
+  const wordToGuessMap = useMemo(
+    () => wordToCharIdxMap(wordToGuess),
+    [wordToGuess]
   );
 
   console.log({ wordToGuess });
 
+  const resetGame = () => {
+    setWordToGuess(getRandomWord());
+    setGuesses([]);
+  };
+
   const handleKeyUp = ({ key }) => {
     if (key === "Backspace") {
       setCurrentGuess((prev) => prev.substring(0, prev.length - 1));
-    } else if (key === "Enter" && currentGuess.length === WORD_LENGTH) {
-      const newGuesses = [...guesses];
-      newGuesses[newGuesses.length] = currentGuess;
-      setGuesses(newGuesses);
-      setCurrentGuess("");
-    } else if (/[a-z]/.test(key)) {
+    } else if (/[a-z]/.test(key) && currentGuess.length < WORD_LENGTH) {
       setCurrentGuess((prev) => prev + key);
+    } else if (key === "Enter" && currentGuess.length === WORD_LENGTH) {
+      setGuesses((prev) => [...prev, currentGuess]);
+      setCurrentGuess("");
     }
   };
 
   useEffect(() => {
     document.addEventListener("keyup", handleKeyUp);
     return () => document.removeEventListener("keyup", handleKeyUp);
-  }, [currentGuess, guesses]);
+  }, [currentGuess]);
 
   useEffect(() => {
-    const won = guesses.some((word) => word === wordToGuess);
+    const won = guesses[guesses.length - 1] === wordToGuess;
     const lost = guesses.length === NUMBER_OF_TRYS;
-    if (won || lost) {
-      setTimeout(() => {
-        alert(won ? "YOU WON :)" : "YOU LOST :(");
-        setWordToGuess(getRandomWord());
-        setGuesses([]);
-      }, DELAY_TO_RESET_GAME);
-    }
+    if (won) toast.success("YOU WON :)");
+    else if (lost) toast.error("YOU LOST :(");
   }, [guesses]);
+
+  useEffect(() => {
+    const unsubscribe = toast.onChange(({ status }) => {
+      if (status === "removed") resetGame();
+    });
+
+    return unsubscribe;
+  }, []);
+
+  const getGuessStatusBgClass = (char, index) => {
+    const isInWord = wordToGuessMap.has(char);
+    const isInWordAtCrtPos = isInWord && wordToGuessMap.get(char).has(index);
+    if (isInWordAtCrtPos) return styles["green-bg"];
+    if (isInWord) return styles["coral-bg"];
+    return styles["gray-bg"];
+  };
+
+  const renderChar = (word, reveal, _, idx) => {
+    const char = word.charAt(idx);
+
+    return (
+      <div key={idx} className={styles.character}>
+        <div className={`${styles.inner} ${reveal ? styles.reveal : ""}`}>
+          <div className={styles.front}>{char}</div>
+          <div className={`${styles.back} ${getGuessStatusBgClass(char, idx)}`}>
+            {char}
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const renderWord = (_, idx) => {
+    const word = guesses[idx] || (idx === guesses.length ? currentGuess : "");
+    const reveal = guesses[idx];
+
+    return (
+      <div key={idx} className={styles.word}>
+        {Array(WORD_LENGTH).fill().map(renderChar.bind(null, word, reveal))}
+      </div>
+    );
+  };
 
   return (
     <div className={styles.container}>
+      <ToastContainer theme="dark" />
       <h1>WORDLE</h1>
-      {Array(NUMBER_OF_TRYS)
-        .fill("")
-        .map((_, i) => (
-          <div key={i} className={styles.word}>
-            {Array(WORD_LENGTH)
-              .fill()
-              .map((_, j) => {
-                const word =
-                  guesses[i] || (i === guesses.length ? currentGuess : "");
-                const char = word.charAt(j);
-                const charInnerClass = `${styles.inner} ${
-                  guesses[i]?.length === WORD_LENGTH ? styles.reveal : ""
-                }`;
-                const backgroundClrClass = char
-                  ? styles["with-blue-background"]
-                  : "";
-                const charFrontClass = `${styles.front} ${backgroundClrClass}`;
-                const charBackClass = `${styles.back} ${backgroundClrClass} ${
-                  wordToGuessSet.has(char)
-                    ? styles["with-coral-background"]
-                    : ""
-                } ${
-                  wordToGuessSetWithIndex.has(`${char}-${j}`)
-                    ? styles["with-green-background"]
-                    : ""
-                }`;
-
-                return (
-                  <div key={j} className={styles.character}>
-                    <div className={charInnerClass}>
-                      <div className={charFrontClass}>{char}</div>
-                      <div className={charBackClass}>{char}</div>
-                    </div>
-                  </div>
-                );
-              })}
-          </div>
-        ))}
+      {Array(NUMBER_OF_TRYS).fill("").map(renderWord)}
     </div>
   );
 };
--- src/pages/Wordle/words.js
+++ src/pages/Wordle/words.js
@@ -1,25 +1,32 @@
 const words = [
-  'route',
-  'crash',
-  'clown',
-  'shaky',
-  'vegan',
-  'power',
-  'trust',
-  'enjoy',
-  'brain',
-  'adopt',
-  'tower',
-  'shade',
-  'delay',
-  'twist',
-  'alert',
-  'choke',
-  'split',
-  'rhyme',
-  'muddy',
-  'plant'
-]
+  "route",
+  "crash",
+  "clown",
+  "shaky",
+  "vegan",
+  "power",
+  "trust",
+  "enjoy",
+  "brain",
+  "adopt",
+  "tower",
+  "shade",
+  "delay",
+  "twist",
+  "alert",
+  "choke",
+  "split",
+  "rhyme",
+  "muddy",
+  "plant",
+];
 
 export const getRandomWord = () =>
-  words[Math.floor(Math.random() * words.length)]
+  words[Math.floor(Math.random() * words.length)];
+
+export const wordToCharIdxMap = (word) =>
+  [...word].reduce((acc, cur, i) => {
+    if (!acc.has(cur)) acc.set(cur, new Set());
+    acc.get(cur).add(i);
+    return acc;
+  }, new Map());
